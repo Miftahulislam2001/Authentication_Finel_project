20381a60b2f3c7681325540808988e55e9da66e5
miftahullislam <miftahullislamsohan@gmail.com>
2024-01-14 19:33:57 +0600

    Authentication Project Initialization

---
+++ src/Components/Register/Register.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Register = () => {
+    return (
+        <div>
+            <h2>This is Register Components</h2>
+        </div>
+    );
+};
+
+export default Register;
